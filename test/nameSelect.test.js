33f7fc1451eb49b3c6f936259bd920b1ad246c06
Persis Ratouis <pratouis@users.noreply.github.com>
2024-09-12 10:04:59 -0700

    adding test files

---
+++ test/nameSelect.test.js
@@ -0,0 +1,10 @@
+import { drawNames } from '../nameSelect';
+import { names1, names2 } from './data.json';
+
+const assert = require('assert');
+
+describe('drawNames', () => {
+    describe('logs when name not found', () => {
+
+    });
+});
