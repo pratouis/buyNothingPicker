1379c548ff13326f23952e5f2d3a0b431f150c0e
Persis Ratouis <persita@Persiss-MBP.attlocal.net>
2024-09-27 14:17:59 -0500

    fixing tracking issues

--- test/nameSelect.test.js
+++ test/nameSelect.test.js
@@ -1,18 +1,21 @@
-const { addNames } = require('../nameSelect');
+const assert = import('assert');
+
+const { addNames } = import('../nameSelect');
 const { names1, names2 } = import('./data.json');
 
-const assert = require('assert');
 
 describe('drawNames', () => {
     describe('adds names', () => {
-        nameDict = new Map();
+        let nameDict = new Map();
         for(someName in names1) {
             assert.doesNotThrow(addNames(nameDict, someName));
         }
     });
 
     describe('does not allow to add duplicates names', () => {
-
+        let nameDict = new Map();
+        names1.forEach(nameKey => nameDict.set(nameKey, []));
+        assert.
     });
 });
 
